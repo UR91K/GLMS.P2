5b54a0030cc20d2ac372296965b073e0d33d5281
Jude Fanner <judeefanner@gmail.com>
2026-04-19 02:39:45 +0200

    feature: add Client, Contract, and ServiceRequest models

---
+++ GLMS.Web/Models/Client.cs
@@ -0,0 +1,11 @@
+namespace GLMS.Web.Models;
+
+public class Client
+{
+    public int ClientId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+
+    public List<Contract> Contracts { get; set; } = [];
+}
