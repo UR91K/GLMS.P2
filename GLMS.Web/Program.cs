5d29b9e1051d9398f0f8cb094c6e42ca32260d7c
Jude Fanner <judeefanner@gmail.com>
2026-04-19 02:47:16 +0200

    chore: add initial database migration and setup

--- GLMS.Web/Program.cs
+++ GLMS.Web/Program.cs
@@ -1,10 +1,15 @@
 using GLMS.Web.Components;
+using GLMS.Web.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<AppDbContext>(opts =>
+    opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
