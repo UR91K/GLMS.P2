ddabcf3e6301eea7e9797450dd68a515c827342a
Jude Fanner <judeefanner@gmail.com>
2026-04-19 02:45:14 +0200

    feature: add ContractStatusChangedEvent class for contract status notifications

---
+++ GLMS.Web/Patterns/Observer/ContractStatusChangedEvent.cs
@@ -0,0 +1,13 @@
+namespace GLMS.Web.Patterns.Observer;
+
+public class ContractStatusChangedEvent
+{
+    public int ContractId { get; }
+    public string NewStatus { get; }
+
+    public ContractStatusChangedEvent(int contractId, string newStatus)
+    {
+        ContractId = contractId;
+        NewStatus = newStatus;
+    }
+}
