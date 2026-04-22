985e0189163fc8fb2fceb5f87c509e1265da5b1b
Jude Fanner <judeefanner@gmail.com>
2026-04-22 11:43:19 +0200

    chore: migrate database

--- GLMS.Web/Migrations/20260422093428_AddRegionAndServiceLevel.Designer.cs
+++ GLMS.Web/Migrations/20260422093428_AddRegionAndServiceLevel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GLMS.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GLMS.Web.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260422093428_AddRegionAndServiceLevel")]
+    partial class AddRegionAndServiceLevel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.6");
---
+++ GLMS.Web/Migrations/20260422093428_AddRegionAndServiceLevel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GLMS.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRegionAndServiceLevel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceLevel",
+                table: "Contracts",
+                type: "TEXT",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Clients",
+                type: "TEXT",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServiceLevel",
+                table: "Contracts");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Clients");
+        }
+    }
+}
